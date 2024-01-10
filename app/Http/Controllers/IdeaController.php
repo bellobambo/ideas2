29db97ddda30feadaded7aa41edeb6f82e3a125e
bellobambo <bellobambo21@gmail.com>
2024-01-10 06:04:22 +0100

    update

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -19,11 +19,11 @@ class IdeaController extends Controller
     {
 
         request()->validate([
-            'idea' => 'required|min:5|max:240'
+            'content' => 'required|min:5|max:240'
         ]);
 
         $idea = Idea::create([
-            'content' => dump(request()->get('idea', '')),
+            'content' => dump(request()->get('content', '')),
         ]);
 
         return redirect()->route('dashboard')->with('success', 'Idea was created successfully!');
@@ -37,4 +37,25 @@ class IdeaController extends Controller
         return redirect()->route('dashboard')->with('success', 'Idea was Deleted successfully!');
 
     }
+
+    public function edit(Idea $idea)
+    {
+        $editing = true;
+
+        return view('ideas.show', compact('idea', 'editing'));
+    }
+    public function update(Idea $idea)
+    {
+        request()->validate([
+            'content' => 'required|min:5|max:240'
+        ]);
+
+        $idea->content = request()->get('content', '');
+        $idea->save();
+
+        return redirect()->route('ideas.show' , $idea->id)->with('success', 'Idea was Updated successfully!');
+
+    }
+
+
 }
